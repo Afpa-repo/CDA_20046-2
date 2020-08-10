6d003720ef4b3c7927111215b18f912030d16ad7
Hijoxx <mr.leroyflorian@gmail.com>
2020-08-10 21:26:16 +0200

    reglage des table / migration

--- symfony/bdd/insert.sql
+++ symfony/bdd/insert.sql
@@ -41,32 +41,32 @@ VALUES (1, 3, "Argentine", "Buenos Aires", "B1636FDA", "La Plata", "230", "Casil
        (17, 2, "Espagne", "Asturias", "33001", "Oviedo", "56", "Calle Avellanos"),
        (18, 2, "Allemagne", "Brandenburg", "10115", "Berlin", "7", "Stremitzer Street");
 
-INSERT INTO `suppliers` (id, adress_id, suppli_company_name, suppli_mail, suppli_phone)
-VALUES (1, 17, "WoodenPC", "WoodenPC@pasfacile.com", "09.75.63.14.78"),
-       (2, 18, "LegoLegends", "LegoLegends@lol.com", "09.76.29.43.86"),
-       (3, 19, "RitoPlease", "RitoPlease@vanvan.com", "09.12.63.45.56"),
-       (4, 20, "AucuneIdée", "AucuneIdée@pls.com", "09.52.45.86.96");
+INSERT INTO `suppliers` (id,  suppli_company_name, suppli_mail, suppli_phone)
+VALUES (1,  "WoodenPC", "WoodenPC@pasfacile.com", "09.75.63.14.78"),
+       (2, "LegoLegends", "LegoLegends@lol.com", "09.76.29.43.86"),
+       (3,  "RitoPlease", "RitoPlease@vanvan.com", "09.12.63.45.56"),
+       (4, "AucuneIdée", "AucuneIdée@pls.com", "09.52.45.86.96");
 
 
 
 INSERT INTO `user` (id, user_titre, user_role_id, user_last_name, user_first_name, user_phone, user_birthday,
-                    user_gender)
-VALUES (1, "client", 1, "Jean", "Bon", "06.15.62.75.95", "1999-08-04", "Homme"),
-       (2, "client", 1, "Jason", "Tatame", "06.78.65.14.23", "1997-12-07", "Homme"),
-       (3, "client", 1, "Clara", "Gartax", "06.98.45.85.96", "1967-04-11", "Femme"),
-       (4, "client", 1, "Alain", "Deloin", "06.78.25.36.14", "1998-07-05", "Homme"),
-       (5, "employé", 2, "Florent", "Panini", "06.65.45.86.96", "1987-09-04", "Homme"),
-       (6, "employé", 2, "Eve", "Tartampion", "06.15.83.27.64", "1975-06-26", "Femme"),
-       (7, "employé", 2, "Brian", "Kitchen", "06.68.21.12.33", "1982-10-29", "Homme"),
-       (8, "employé", 2, "Sarah", "Conor", "06.11.12.54.86", "1996-11-12", "Femme"),
-       (9, "commercial", 3, "Lucas", "Niche", "06.89.45.63.12", "1985-09-07", "Homme"),
-       (10, "commercial", 3, "Enzo", "Batelli", "06.78.65.23.23", "1987-07-26", "Homme"),
-       (11, "commercial", 3, "Jean-Michel", "Apeuprès", "06.45.80.69.32", "1999-09-06", "Homme"),
-       (12, "commercial", 3, "Giselle", "Lelustre", "06.00.23.58.58", "1981-12-04", "Femme"),
-       (13, "logisticien", 5, "Julien", "Emerite", "06.78.56.12.96", "1986-03-11", "Homme"),
-       (14, "logisticien", 5, "Marjorie", "Brie", "06.55.66.78.25", "1985-12-10", "Femme"),
-       (15, "logisticien", 5, "Audrey", "Carto", "06.56.21.40.80", "1972-02-02", "Femme"),
-       (16, "logisticien", 5, "Yohann", "Grif", "06.12.07.93.20", "1974-07-06", "Homme");
+                    user_gender,email,password)
+VALUES (1, "client", 1, "Jean", "Bon", "06.15.62.75.95", "1999-08-04", "Homme","test@test.com","sgfjhsdgois"),
+       (2, "client", 1, "Jason", "Tatame", "06.78.65.14.23", "1997-12-07", "Homme","test@test.com","sgfjhsdgois"),
+       (3, "client", 1, "Clara", "Gartax", "06.98.45.85.96", "1967-04-11", "Femme","test@test.com","sgfjhsdgois"),
+       (4, "client", 1, "Alain", "Deloin", "06.78.25.36.14", "1998-07-05", "Homme","test@test.com","sgfjhsdgois"),
+       (5, "employé", 2, "Florent", "Panini", "06.65.45.86.96", "1987-09-04", "Homme","test@test.com","sgfjhsdgois"),
+       (6, "employé", 2, "Eve", "Tartampion", "06.15.83.27.64", "1975-06-26", "Femme","test@test.com","sgfjhsdgois"),
+       (7, "employé", 2, "Brian", "Kitchen", "06.68.21.12.33", "1982-10-29", "Homme","test@test.com","sgfjhsdgois"),
+       (8, "employé", 2, "Sarah", "Conor", "06.11.12.54.86", "1996-11-12", "Femme","test@test.com","sgfjhsdgois"),
+       (9, "commercial", 3, "Lucas", "Niche", "06.89.45.63.12", "1985-09-07", "Homme","test@test.com","sgfjhsdgois"),
+       (10, "commercial", 3, "Enzo", "Batelli", "06.78.65.23.23", "1987-07-26", "Homme","test@test.com","sgfjhsdgois"),
+       (11, "commercial", 3, "Jean-Michel", "Apeuprès", "06.45.80.69.32", "1999-09-06", "Homme","test@test.com","sgfjhsdgois"),
+       (12, "commercial", 3, "Giselle", "Lelustre", "06.00.23.58.58", "1981-12-04", "Femme","test@test.com","sgfjhsdgois"),
+       (13, "logisticien", 5, "Julien", "Emerite", "06.78.56.12.96", "1986-03-11", "Homme","test@test.com","sgfjhsdgois"),
+       (14, "logisticien", 5, "Marjorie", "Brie", "06.55.66.78.25", "1985-12-10", "Femme","test@test.com","sgfjhsdgois"),
+       (15, "logisticien", 5, "Audrey", "Carto", "06.56.21.40.80", "1972-02-02", "Femme","test@test.com","sgfjhsdgois"),
+       (16, "logisticien", 5, "Yohann", "Grif", "06.12.07.93.20", "1974-07-06", "Homme","test@test.com","sgfjhsdgois");
 
 
 
@@ -102,27 +102,7 @@ INSERT INTO `products` (id, pro_name, pro_stock_ale, pro_unit_price, pro_unit_st
 
 
 
-INSERT INTO `products_material` (products_id, material_id)
-    VALUE (1, 2),
-    (2, 3),
-    (3, 2),
-    (4, 1),
-    (2, 4),
-    (5, 3),
-    (6, 3),
-    (7, 2);
-
-INSERT INTO `products_format` (products_id, format_id)
-    VALUE (1, 2),
-    (2, 3),
-    (3, 3),
-    (4, 4),
-    (2, 2),
-    (5, 1),
-    (6, 1),
-    (7, 3);
-
-INSERT INTO `products_theme` (products_id, theme_id)
+INSERT INTO `theme_products` (products_id, theme_id)
     VALUE
     (1, 1),
     (2, 2),
@@ -133,12 +113,12 @@ INSERT INTO `products_theme` (products_id, theme_id)
     (7, 2);
 
 
-INSERT INTO `order` (id,order_date, order_date_shipping, order_type, order_shipping_cost,orderdetail_id)
+INSERT INTO `order` (id,order_date, order_date_shipping, order_type, order_shipping_cost,user_id)
 VALUES (1, "2020-02-15", "2020-02-27", 1, 20,1),
        (2, "2020-03-27", "2020-04-5", 2, 5,2),
        (3, "2020-01-01", "2020-01-12", 2, 7,3),
        (4, "2020-06-07", "2020-06-18", 2, 5,4),
-       (5, "2020-03-10", "2020-03-14", 2, 3,5);
+       (5, "2020-03-10", "2020-03-14", 2, 3,4);
 
 
 INSERT INTO `order_adress` (order_id, adress_id)
@@ -150,12 +130,12 @@ VALUES (1, 1),
        (5, 1);
 
 
-INSERT INTO `orders_details` (id,  orderdetails_unit_price, orderdetails_quantity, ordersdetails_discount,ordersdetails_tva,products_id)
-    VALUE   (1,  30, 10, 5,20,1),
-    (4,  30, 2, 20,20,1),
-    (2,  30, 2, 20,20,1),
-    (3,  30, 2, 20,20,1),
-    (5,  30, 2, 20,20,1);
+INSERT INTO `orders_details` (id,  orderdetails_unit_price, orderdetails_quantity, ordersdetails_discount,ordersdetails_tva,product_id,orders_id)
+    VALUE   (1,  30, 10, 5,20,1,1),
+    (4,  30, 2, 20,20,1,2),
+    (2,  30, 2, 20,20,1,3),
+    (3,  30, 2, 20,20,1,3),
+    (5,  30, 2, 20,20,1,4);
 
 INSERT INTO `orders_details` (id,  orderdetails_unit_price, orderdetails_quantity,ordersdetails_tva)
     VALUE   (6 ,20, 1,20),
