3d220325600f4d02fe102f52f400f8b42503b264
hijoxx <mr.leroyflorian@gmail.com>
2020-08-18 11:00:04 +0200

    fix deS route

--- filRouge/src/Controller/UserController.php
+++ filRouge/src/Controller/UserController.php
@@ -93,7 +93,7 @@ class UserController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
             $this->getDoctrine()->getManager()->flush();
 
-            return $this->redirectToRoute('user_index');
+            return $this->redirectToRoute('home');
         }
 
         return $this->render('user/edit.html.twig', [
