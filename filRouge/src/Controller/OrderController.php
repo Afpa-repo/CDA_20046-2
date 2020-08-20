31f6ca0652ae9aa80fe8dd43d9c790ea23e0766d
ptyga <pigeaud.jeremy@gmail.com>
2020-08-20 13:52:09 +0200

    nada

--- filRouge/src/Controller/OrderController.php
+++ filRouge/src/Controller/OrderController.php
@@ -23,44 +23,58 @@ class OrderController extends AbstractController
         ]);
     }
 
-    /**
-     * @Route("/order/submit", name="order_submit")
-     */
-    public function submit(CartService $cartService)
-    {
-
-        $cartService = $cartService->getFullCart();
-
-
-            dd($cartService[0]);
+    
+    public function load(ObjectManager $manager, $cartService) {
 
         $date = new \DateTime();
         $dateShipping = $date->add(new \DateInterval('P3D'));
 
         $order = new Order();
+        $user = new User(1);
 
-        $order->setUser(new User(1));
+        $order->setUser($user);
         $order->setAddress(new Address(1));
         $order->setOrderDate($date);
         $order->setOrderDateShipping($dateShipping);
         $order->setOrderType(1);
-        $order->setOrderShippingCost(rand(0,100));
+        $order->setOrderShippingCost(rand(0, 100));
 
-        for($i=0;$i<=sizeOf($cartService);$i++) {
+        for ($i = 0; $i <= sizeOf($cartService); $i++) {
 
-            for($j=0;$j<=sizeOf($cartService[$i]["product"]);$j++) {
+            for ($j = 0; $j <= sizeOf($cartService[$i]["product"]); $j++) {
 
                 $orderDetail = new OrderDetail();
 
                 $orderDetail->setProduct($cartService[$i]["product"]->getId($j));
                 $orderDetail->setOrders($order);
                 $orderDetail->setSupplier($cartService[$i]["stock"]->getMaterial()->getSupplier());
-                $orderDetail->setStock($cartService[$i]->getStock());
+                $orderDetail->setStock($cartService[$i]["stock"]->getId());
+                $orderDetail->setOrderdetailUnitPrice($cartService[$i]["stock"]->getUnitPrice());
+                $orderDetail->setOrderdetailQuantity($cartService[$i]["quantity"]);
+                $orderDetail->setOrderdetailDiscount(rand(1, 10) / 10);
 
+                $manager->persist($orderDetail);
             }
         }
-        
-        
+
+        $manager->persist($order);
+
+        $manager->flush();
+
+    }
+
+    /**
+     * @Route("/order/submit", name="order_submit")
+     */
+    public function submit(ObjectManager $manager, CartService $cartService)
+    {
+
+        $cartService = $cartService->getFullCart();
+
+
+            dd($cartService);
+
+        $this->load($manager, $cartService);
 
 
         return $this->render('order/index.html.twig', [
